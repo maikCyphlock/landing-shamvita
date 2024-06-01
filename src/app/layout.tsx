656eeb84eedadecb47f2523b9548a2c28618c35e
maikCyphlock <maikolaguilar656@gmail.com>
2024-06-01 16:06:06 -0400

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   
   return (
     <html lang="en" className="dark">
-      <body >
-        <video src="/bg-sd-ultra.mp4" autoPlay muted loop className="absolute inset-0 w-full h-full object-cover blur-2xl brightness-[0.4]" >
+      <body className="relative">
+        <video src="/bg-sd-ultra.mp4" autoPlay muted loop className="absolute z-0 inset-0 w-full h-full object-cover blur-2xl brightness-[0.4]" >
       
         </video>
         
@@ -30,19 +30,7 @@ export default function RootLayout({
           <p className="text-center text-sm text-gray-500">
             Hecho con ❤️ por <a href="https://www.instagram.com/maikol_aguilar__/" target="_blank" rel="noopener noreferrer" className="underline">Maikol Aguilar</a>
           </p>
-          <p className="text-center text-sm text-gray-500">
-            <a href="https://github.com/maikCyphlock/landing-shamvita" target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
-            <span className="mx-2">|</span>
-            <a href="https://twitter.com/shamvita" target="_blank" rel="noopener noreferrer">
-              Twitter
-            </a>
-            <span className="mx-2">|</span>
-            <a href="https://www.instagram.com/shamvita/" target="_blank" rel="noopener noreferrer">
-              Instagram
-            </a>
-          </p>
+          
         </footer>
         </body>
     </html>
